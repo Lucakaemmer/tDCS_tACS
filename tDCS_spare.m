8365023b1f0a2714f5375a784e55369f71e83e0d
Lucakaemmer <60244347+Lucakaemmer@users.noreply.github.com>
2022-01-21 15:48:18 +0100

    added key response and expanded logfile

--- tDCS_spare.m
+++ tDCS_spare.m
@@ -31,3 +31,31 @@ fclose(fileID)
 
 % Alternative to writing log fiel that just writes it as one in the end
 writematrix(Log_File, 'Outputs/Log_File.tsv', 'FileType', 'text', 'Delimiter', '\t')
+
+
+%% *******************************************************************
+%                            Key Responses
+%*********************************************************************
+
+%*********************
+% Keyboard information
+% % improve portability of your code acorss operating systems 
+KbName('UnifyKeyNames');
+% % specify key names of interest in the study
+% activeKeys = [KbName('LeftArrow') KbName('RightArrow') KbName('ESCAPE')];
+% % set value for maximum time to wait for response (in seconds)
+% t2wait = 1.5; 
+% % if the wait for presses is in a loop, 
+% % then the following two commands should come before the loop starts
+% % restrict the keys for keyboard input to the keys we want
+% RestrictKeysForKbCheck(activeKeys);
+% % suppress echo to the command line for keypresses
+% ListenChar(2);
+% % get the time stamp at the start of waiting for key input 
+% % so we can evaluate timeout and reaction time
+
+escapeKey = KbName('ESCAPE');
+leftKey = KbName('LeftArrow');
+rightKey = KbName('RightArrow');
+
+
